7bd5dd7fe9e3c5ca80298269adcc3ce728f6456a
Antti Sykäri <antti.sykari@gmail.com>
2013-11-03 18:19:20 +0200

    How to keep server connection up.

--- client/client.js
+++ client/client.js
@@ -5,13 +5,37 @@ log(_.range(1, 10));
 
 var socket = new eio.Socket();
 
+socket.on('error', function(err) {
+	connected = false;
+});
+
+var connected = false;
+
+setInterval(function() {
+	if (connected === false) {
+		console.log("connection closed. trying to reopen...");
+		socket.open();
+	}
+}, 1000);
+
+socket.on('message', function(data) {
+	log('Got message:', data);
+	if (data == 'hi') {
+		socket.send('newuser');
+	}
+});
+
+socket.on('close', function() {
+	connected = false;
+	log('Socket closed. Trying again soon.')
+});
+
 socket.on('open', function() {
-	socket.on('message', function(data) { log('Got message!', data); });
-	socket.on('close', function() { log('Socket closed.') });
+	connected = true;
 });
 
 setInterval(function() {
 	player.update();
-//	log("Player position is", player.pos);
+	log("Player position is", player.pos);
 }, 1000);
 
