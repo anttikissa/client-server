bca26c165ca14ffe696c2e16ccd3086362e1d25f
Antti Sykäri <antti.sykari@gmail.com>
2013-11-03 11:42:35 +0200

    Working hacky implementation.

--- public/client.js
+++ public/client.js
@@ -1,5 +1,10 @@
+var player = require('./player');
 
 console.log(_.range(1, 10));
 
+setInterval(function() {
+	player.update();
+	console.log("Player position is ", player.pos);
+}, 500);
 alert('this is client!');
 
---
+++ public/player.js
@@ -0,0 +1,15 @@
+//var log = require('basic-log');
+
+player = {
+	name: 'Bob',
+	pos: [0, 0],
+	score: 0,
+	update: function() {
+//		log.d("player pos ", this.pos, " random walking"); 
+		this.pos[0] += Math.random() - .5;
+		this.pos[1] += Math.random() - .5;
+//		log.d("player pos ", this.pos, " after random walk"); 
+	}
+};
+
+module.exports = player;
