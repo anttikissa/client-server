f0ed2b1779b2085c8148bcde0cf2fc2f46d45345
Antti Sykäri <antti.sykari@gmail.com>
2013-11-03 18:34:36 +0200

    Moving thing on the canvas.

--- client/client.js
+++ client/client.js
@@ -3,6 +3,9 @@ var player = require('./player');
 
 log(_.range(1, 10));
 
+//
+// network
+//
 var socket = new eio.Socket();
 
 socket.on('error', function(err) {
@@ -34,8 +37,37 @@ socket.on('open', function() {
 	connected = true;
 });
 
+var canvas;
+var ctx;
+var canvasWidth;
+var canvasHeight;
+var unitSizePx;
+
+window.onload = function() {
+	canvas = document.querySelector('canvas');
+
+	canvasWidth = canvas.width;
+	canvasHeight = canvas.height;
+	unitSizePx = canvasWidth / 4;
+	ctx = canvas.getContext('2d');
+}
+
+function drawPlayer() {
+	var playerSize = 0.5;
+	var posX = player.pos[0] * unitSizePx + canvasWidth / 2;
+	var posY = player.pos[1] * unitSizePx + canvasHeight / 2;
+	var widthPx = unitSizePx * playerSize;
+	var heightPx = unitSizePx * playerSize;
+	ctx.fillRect(posX, posY, widthPx, heightPx);
+}
+
+//
+// other stuff
+//
 setInterval(function() {
 	player.update();
+	ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+	drawPlayer();
 	log("Player position is", player.pos);
 }, 1000);
 
