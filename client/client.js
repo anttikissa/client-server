2853c8571572e3d2031491de7c5b85dc36ee8689
Antti Sykäri <antti.sykari@gmail.com>
2013-11-03 18:04:00 +0200

    Just open the socket to the default place.

--- client/client.js
+++ client/client.js
@@ -3,7 +3,7 @@ var player = require('./player');
 
 log(_.range(1, 10));
 
-var socket = new eio.Socket('ws://localhost:3000/');
+var socket = new eio.Socket();
 
 socket.on('open', function() {
 	socket.on('message', function(data) { log('Got message!', data); });
